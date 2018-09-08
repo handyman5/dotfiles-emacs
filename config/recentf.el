eb23d3250f523a646026eecdbf6fb648fac70008
Adam Compton <comptona@gmail.com>
2018-09-08 15:16:17 -0700

    removing config

--- config/recentf.el
+++
@@ -1,5 +0,0 @@
-;; recentf stuff -- http://www.joegrossberg.com/archives/000182.html
-(require 'recentf)
-(recentf-mode)
-(setq recentf-max-menu-items 25)
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
