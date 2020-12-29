077b24bf84cb91a2fe0f3a77038715e63e8f63db
Adam Compton <acompton@slack-corp.com>
2020-12-29 14:31:58 -0800

    further refinements

--- init.el
+++ init.el
@@ -4,3 +4,16 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-ui lsp-mode yasnippet flycheck highlight-indentation company-prescient company ivy-prescient counsel ivy-rich ivy which-key smex diminish monokai-pro-theme use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
