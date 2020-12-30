9cbd619467eaeba7abc962d33a3ee61ecee81957
Adam Compton <acompton@slack-corp.com>
2020-12-29 22:37:47 -0800

    changes

--- elpa/mac-pseudo-daemon-20170728.1940/mac-pseudo-daemon-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mac-pseudo-daemon" "20170728.1940" "Daemon mode that plays nice with Mac OS." '((cl-lib "0.1")) :commit "d235680a72677f11925b912428ad1a57b664e3e8" :keywords '("convenience" "osx" "mac") :authors '(("Ryan C. Thompson")) :maintainer '("Ryan C. Thompson") :url "https://github.com/DarwinAwardWinner/osx-pseudo-daemon")
