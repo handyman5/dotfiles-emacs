d05bdd06ea7098fbc4f92e95da81579ca70b9e13
Adam Compton <acompton@slack-corp.com>
2021-01-02 15:21:22 -0800

    revamped

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;; use this as a reference: https://github.com/patrickt/emacs/blob/master/init.el
+
+
+
 ;; speedup tip from https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
 (setq gc-cons-threshold 1000000000)
 
@@ -10,16 +14,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(avy ivy-avy lsp-ui lsp-mode yasnippet flycheck highlight-indentation company-prescient company ivy-prescient counsel ivy-rich ivy which-key smex diminish monokai-pro-theme use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
